113d3a485bdf0e9e0d43d4a48830f084f65e5e1a
jochemwhite <j.vanderwit@amrio.nl>
2023-10-10 10:32:50 +0200

    added start script and fixed the DNS

--- src/grpc/checkVariable.ts
+++ src/grpc/checkVariable.ts
@@ -2,7 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 import { variableServiceClient } from "../proto/checkVariable";
 import * as config from "../config.json";
 
-const host = config.checkVariable.host;
+const host = config.checkVariable.dns;
 const port = config.checkVariable.port;
 const connectionString = `${host}:${port}`;
 
--- src/grpc/spotify.ts
+++ src/grpc/spotify.ts
@@ -2,7 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 import { SpotifyServiceClient } from "../proto/SpotifyAPI";
 import * as config from "../config.json";
 
-const host = config.spotifyAPI.host;
+const host = config.spotifyAPI.dns;
 const port = config.spotifyAPI.port;
 const connectionString = `${host}:${port}`;
 
